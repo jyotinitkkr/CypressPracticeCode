964a2c2b2e0b62ecbd29f729db8194e4e13ce7ff
jyotinitkkr <106717529+jyotinitkkr@users.noreply.github.com>
2022-06-09 14:05:25 -0400

    Add files via upload

---
+++ getUserfile.js
@@ -0,0 +1,47 @@
+/// <reference types = "Cypress" />
+
+describe('Get API test', ()=>{
+    let accessToken = '0ec41d742792722a7f534b3e25ac264f459841bf3a806e531e425db0d1040dfc'
+    it('Get Users', ()=>{
+        cy.request({
+            method: 'GET',
+            url: 'https://gorest.co.in/public-api/users',
+            headers:{
+                'authorization' : "Bearer"+accessToken
+            }
+            }).then((res)=>{
+                expect(res.status).to.eq(200)
+                expect(res.body.meta.pagination.limit).to.eq(20)
+        })
+    })
+    it('Get Users by ID test', ()=>{
+        cy.request({
+            method: 'GET',
+            url: 'https://gorest.co.in/public-api/users',
+            headers:{
+            }
+            }).then((res)=>{
+                expect(res.status).to.eq(200)
+                expect(res.body.data[0].name).to.eq('Chakravartee Gandhi')
+        })
+    })
+    it('GET call', ()=>{
+
+        // By default if method name is not mentioned in request call, it is considered as GET request.
+        cy.request('https://flask-rest-api-demo.herokuapp.com/product/motorbike').then((res)=>{
+        expect(res.body.product[0]).have.property('product','motorbike')
+        })
+    })
+    it('GET users', ()=>{
+        cy.request({
+            method:'GET',
+            url:'https://flask-rest-api-demo.herokuapp.com/users'
+        }).then((res)=>{
+            expect(res.status).equal(200),
+            
+            expect(res.body.users[0]).have.property('username')
+            
+        })
+    })
+})
+
